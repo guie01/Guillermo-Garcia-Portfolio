fec5dd8ba7ea6ea34bd87ea0db09cff19ed3baed
Guillermo Garcia <ggq0107@hotmail.com>
2021-02-13 19:43:36 -0500

    added portfolio work, css is confusing must redo

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+document.getElementById("project-repo").onclick = function () {
+    location.href = "https://github.com/guie01/Project-W7-Group-5#application";
+}
+document.getElementById("project-deployed").onclick = function () {
+    location.href = "https://guie01.github.io/Project-W7-Group-5/";
+}
+document.getElementById("weather-repo").onclick = function () {
+    location.href = "https://github.com/guie01/Weather-Web-App";
+}
+document.getElementById("weather-deployed").onclick = function () {
+    location.href = "https://guie01.github.io/Weather-Web-App/";
+}
