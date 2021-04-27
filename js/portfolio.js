57579c6355cdf6fba04995c0695e29caf25a9b6f
Guillermo Garcia <ggq0107@hotmail.com>
2021-04-27 13:27:13 -0400

    btn, repo and deployed app second project

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,3 +10,9 @@ document.getElementById("weather-repo").onclick = function () {
 document.getElementById("weather-deployed").onclick = function () {
     location.href = "https://guie01.github.io/Weather-Web-App/";
 }
+document.getElementById("dnd-deployed").onclick = function () {
+    location.href = "https://radiant-island-35045.herokuapp.com/";
+}
+document.getElementById("dnd-repo").onclick = function () {
+    location.href = "https://github.com/Nickpodski/My_DND_SpellBook";
+}
